2bdd6477736e362e9aca97ea3494cb1214ed36d9
Oliver Maaker <maaker@gmail.com>
2020-08-04 12:10:19 +0300

    stylehack components

--- StyleHack.js
+++ StyleHack.js
@@ -1,11 +1,7 @@
 import {Css} from "https://designstem.github.io/fachwerk/fachwerk.js";
-
 export default {
-  props: {
-    tint: { default: "hsl(0, 100%, 100%)", type: [String] },
-  },
   mixins: [Css],
-  template: ``,
+  template: ` `,
   css: `
   html.a-fullscreen {
     position: relative;
--- StyleHackFp.js
+++ StyleHackFp.js
@@ -2,7 +2,7 @@ import {Css} from "https://designstem.github.io/fachwerk/fachwerk.js";
 
 export default {
   mixins: [Css],
-  template: ``,
+  template: ` `,
   css: `
   html.a-fullscreen {
     position: fixed;
